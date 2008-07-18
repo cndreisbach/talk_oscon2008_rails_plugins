05152d4d2c1e03d7b0f81f8d62fb51d036a7665c
Clinton R. Nixon <clinton.nixon@viget.com>
2008-07-18 16:00:03 -0400

    finished tutorial

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 rails rails_app -d sqlite3
 cd rails_app
+rake rails:freeze:gems
 script/plugin install git://github.com/mbleigh/acts-as-taggable-on.git
 script/plugin install http://svn.techno-weenie.net/projects/plugins/acts_as_paranoid/
 script/plugin install http://elitists.textdriven.com/svn/plugins/acts_as_state_machine/trunk/
 script/plugin install http://svn.techno-weenie.net/projects/plugins/acts_as_versioned/
 script/plugin install http://svn.extendviget.com/lab/bloget/trunk/
 script/plugin install http://svn.rubyonrails.org/rails/plugins/exception_notification/
-script/plugin install git://github.com/vigetlabs/foreign_key_migrations.git
+script/plugin install git://github.com/crnixon/foreign_key_migrations.git
 script/plugin install http://ennerchi.googlecode.com/svn/trunk/plugins/jrails
+svn export http://pseudocursors.rubyforge.org/svn/tags/stable/ vendor/plugins/pseudocursors
 script/plugin install git://github.com/jnewland/resource_this.git
 script/plugin install git://github.com/technoweenie/restful-authentication.git
 script/plugin install git://github.com/thoughtbot/shoulda.git
