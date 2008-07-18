eac9bed3ba64a126cb9d989a82fb0f6128982689
Clinton R. Nixon <clinton.nixon@viget.com>
2008-07-18 17:17:37 -0400

    made most of the plugins pull from git

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@ rails rails_app -d sqlite3
 cd rails_app
 rake rails:freeze:gems
 script/plugin install git://github.com/mbleigh/acts-as-taggable-on.git
-script/plugin install http://svn.techno-weenie.net/projects/plugins/acts_as_paranoid/
+script/plugin install git://github.com/technoweenie/acts_as_paranoid.git
 script/plugin install http://elitists.textdriven.com/svn/plugins/acts_as_state_machine/trunk/
-script/plugin install http://svn.techno-weenie.net/projects/plugins/acts_as_versioned/
-script/plugin install http://svn.extendviget.com/lab/bloget/trunk/
-script/plugin install http://svn.rubyonrails.org/rails/plugins/exception_notification/
-script/plugin install git://github.com/crnixon/foreign_key_migrations.git
-script/plugin install http://ennerchi.googlecode.com/svn/trunk/plugins/jrails
+script/plugin install git://github.com/technoweenie/acts_as_versioned.git
+script/plugin install git://github.com/vigetlabs/bloget.git
+script/plugin install git://github.com/rails/exception_notification.git
+script/plugin install git://github.com/vigetlabs/foreign_key_migrations.git
+script/plugin install git://github.com/aaronchi/jrails.git
 svn export http://pseudocursors.rubyforge.org/svn/tags/stable/ vendor/plugins/pseudocursors
 script/plugin install git://github.com/jnewland/resource_this.git
 script/plugin install git://github.com/technoweenie/restful-authentication.git
 script/plugin install git://github.com/thoughtbot/shoulda.git
-script/plugin install http://svn.rubyonrails.org/rails/plugins/ssl_requirement/
+script/plugin install git://github.com/rails/ssl_requirement.git
 script/plugin install http://pmade.com/svn/oss/stickies/trunk
 script/generate plugin test_plugin
 cp ../webmate script/webmate
